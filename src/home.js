689b48857bf1e890ca1bdfe4df28257b0d1c45ec
MinhDucMa97 <ducminh4497@gmail.com>
2024-05-20 19:11:33 -0700

    Imlementing CSS animation for home page

--- src/home.js
+++ src/home.js
@@ -1,17 +1,25 @@
 import mainPageImage from "./main_page_image.png";
 
-export function homePageLoading() {
+export default function homePageLoading() {
+	const mainContainer = document.getElementById("content");
+
 	const contentContainer = document.createElement("div");
 	contentContainer.className = "home-page-container";
+	contentContainer.id = "home-page-container";
 
 	const leftContainer = document.createElement("div");
+	leftContainer.id = "left-container";
+	leftContainer.className = "left-container";
 	const pageImage = document.createElement("img");
 	pageImage.src = mainPageImage;
-	leftContainer.className = "left-container";
 	leftContainer.appendChild(pageImage);
 
 	const rightContainer = document.createElement("div");
 	rightContainer.className = "right-container";
+	rightContainer.id = "right-container";
 
-	contentContainer.appendChild(leftContainer, rightContainer);
+	contentContainer.appendChild(leftContainer);
+	contentContainer.appendChild(rightContainer);
+
+	mainContainer.appendChild(contentContainer);
 }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import homePage, { homePageLoading } from "./home";
 import "./style.css";
 import Logo from "./logo.jpg";
 import homePageLoading from "./home.js";
 
 const logoImage = document.getElementById("logo-img");
 logoImage.src = Logo;
+logoImage.addEventListener("click", homePageLoading);
 
 document.addEventListener("DOMContentLoaded", () => {});
