5ccc6a016d773a911d9119a4358b07cfb29f8c72
MinhDucMa97 <ducminh4497@gmail.com>
2024-06-14 16:25:52 -0700

    Menu Page Implementation

--- src/menu.js
+++ src/menu.js
@@ -1,9 +1,28 @@
 export default function menuPageLoading() {
-  const mainContainer = document.getElementById("content");
+	const menuSelection = [
+		{ name: "Beef Tenderloin Steak Salad", price: "$15.50" },
+		{ name: "Pho Special", price: "$17.50" },
+		{ name: "Grilled Pork Chop & Grilled Shrimp Rice Plate", price: "$17.95" },
+	];
 
-  const menuPageContainer = document.createElement("div");
-  menuPageContainer.className = "menu_page_container";
-  menuPageContainer.id = "menu_page_container";
+	const mainContainer = document.getElementById("content");
 
-  mainContainer.appendChild(menuPageContainer);
+	mainContainer.innerHTML = "";
+
+	const menuPageContainer = document.createElement("div");
+	menuPageContainer.className = "menu_page_container";
+	menuPageContainer.id = "menu_page_container";
+
+	const menuHeader = document.createElement("h1");
+	menuHeader.className = "menu-header";
+	menuHeader.id = "menu-header";
+	menuHeader.textContent = "Menu";
+	menuPageContainer.appendChild(menuHeader);
+
+	const menuContent = document.createElement("div");
+	menuContent.className = "menu-content";
+	menuContent.id = "menu-content";
+	menuPageContainer.appendChild(menuContent);
+
+	mainContainer.appendChild(menuPageContainer);
 }
